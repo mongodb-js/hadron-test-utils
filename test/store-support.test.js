ecbe2ba25093f383aa489c00265a59323bc56739
Durran Jordan <durran@gmail.com>
2016-04-04 22:14:28 +0200

    Fixing store tests

--- test/store-support.test.js
+++ test/store-support.test.js
@@ -17,17 +17,17 @@ describe('StoreSupport', function() {
     it('sets up the data service and namespace', function(done) {
       StoreSupport.setup(DATABASE, COLLECTION, CONNECTION, function() {
         expect(ApplicationStore.dataService).to.not.equal(null);
-        expect(NamespaceStore.namespace).to.equal('test.users');
+        expect(NamespaceStore.ns).to.equal('test.users');
         done();
       });
     });
   });
 
-  describe('#teardown', funciton() {
+  describe('#teardown', function() {
     it('tears down the data service and namespace', function(done) {
       StoreSupport.teardown(function() {
         expect(ApplicationStore.dataService).to.equal(null);
-        expect(NamespaceStore.namespace).to.equal(null);
+        expect(NamespaceStore.ns).to.equal(null);
         done();
       });
     });
