f004ae03242ac815756c490d5c620dba18c41141
Durran Jordan <durran@gmail.com>
2016-05-02 12:23:05 -0400

    Set NODE_ENV to testing is not provided

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'testing';
+}
+
 module.exports.CrudSupport = require('./lib/crud-support');
 module.exports.StoreSupport = require('./lib/store-support');
 module.exports.SpectronSupport = require('./lib/spectron-support');
